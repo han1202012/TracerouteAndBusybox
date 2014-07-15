b0b1c440ea71d1dd26d898d96c9da330c9526e6b
HanShuliang <13241153187@163.com>
2014-07-15 21:34:21 +0800

    first commit

---
+++ gen/cn/org/octopus/tracerouteandbusybox/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package cn.org.octopus.tracerouteandbusybox;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
